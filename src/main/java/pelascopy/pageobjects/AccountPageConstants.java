735c49d358228a3ccbf91f7c749edfd5ea891bdb
prerana9211 <prerana.shrivastava@hotmail.com>
2017-09-20 10:39:37 +0530

    first commit

---
+++ src/main/java/pelascopy/pageobjects/AccountPageConstants.java
@@ -0,0 +1,18 @@
+package pelascopy.pageobjects;
+
+public class AccountPageConstants {
+	public static String Acctab= ".//li[@id='Account_Tab']/a";
+	public static String AccNewBtn = ".//input[@type='button' and @value=' New ']";
+	public static String Acc_type= ".//select[@id='p3']";
+	public static String Continue_btn =".//input[@type='submit' and @value='Continue']";
+	public static String Account_Name=".//input[@id='acc2']";
+	public static String Comapny_legal_Name=".//input[@id='00N2400000FBiLw']";
+	public static String Description=".//input[@id='acc20' and @class='requiredInput']";
+	public static String Master_agreement_no=".//input[@id='00N2400000FBiM1']";
+	public static String save_btn=".//input[@type='submit' and @value=' Save ']";
+	public static String New_contact_btn=".//input[@type='button' and @value='New Contact']";
+	public static String New_oppty=".//input[@value='New Opportunity']";
+	public static String Manage_PP=".//div[@id='workWithPortalButton']";
+	public static String Contact_link=".//span[@class='listTitle' and contains(text(),'Contacts')]";
+}
+////*[@id="acc20"]
---
+++ src/main/java/pelascopy/pageobjects/OpportunityPageConstant.java
@@ -0,0 +1,7 @@
+package pelascopy.pageobjects;
+
+public class OpportunityPageConstant {
+	public static String Oppty_Name=".//input[@id='opp3' and @type='text']";
+	public static String Close_Date=".//span[@class='dateInput dateOnlyInput']";
+	public static String save_btn=".//input[@type='submit' and @value=' Save ']";
+}
